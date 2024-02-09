12bbad5ae2a44fed603ee0975ba50033443cf9f5
Kesha <kesha@gmail.com>
2024-02-09 22:50:03 +0600

    lab3

---
+++ lab3/Exercise11.py
@@ -0,0 +1,14 @@
+def is_palindrome(s):
+    alllow = ''.join(s.lower().split())
+    if(alllow == alllow[::-1]):
+        return True
+    else:
+        return False
+
+examp = input()
+result = is_palindrome(examp)
+
+if result:
+    print("It's a palindrome")
+else:
+    print("It's not a palindrome")
